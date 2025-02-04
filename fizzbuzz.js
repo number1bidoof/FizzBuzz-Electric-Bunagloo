0fd3f62d47b6fe17d5d1b304c815655a890ba4bd
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2025-02-04 01:54:59 -0500

    Almost done with FizzBuzz pt.2

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+// Defines all varribles from HTMl
+const MyNum = document.getElementById("numput")
+const MyResult = document.getElementById("result")
+const MyButton = document.getElementById("enter")
+let i
+let fizz = "Fizz"
+let buzz = "Buzz"
+MyButton.onclick = function() {
+    //Event.preventDefault()
+    i = MyNum.value;
+        if (i % 3 === 0 && i % 5 === 0) {
+            MyResult.innerHTML = fizz + buzz; // or "FizzBuzz"
+        } else if (i % 3 === 0) {
+            MyResult.innerHTML = fizz;
+        } else if (i % 5 === 0) {
+            MyResult.innerHTML = buzz;
+        } else {
+            MyResult.innerHTML = i
+        }
+}
